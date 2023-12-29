9b0807bc7629769053faf62949b03342b38b157f
sayoi341 <me@sayoi341.moe>
2023-12-29 20:49:13 +0900

    ????

--- install.sh
+++ install.sh
@@ -2,12 +2,10 @@
 
 set -eufo pipefail
 
-echo ""
 echo "🤚  This script will setup .dotfiles for you."
 
 # Install Homebrew
 command -v brew >/dev/null 2>&1 || \
     (echo '🍺  Installing Homebrew' && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)")
 
-echo ""
 echo "Done."
