fabbc9dc25d8d826019ad8376694581e7fad2f7a
sayoi341 <me@sayoi341.moe>
2023-12-29 21:22:37 +0900

    Add install script and setup container script

--- setup_container.sh
+++ setup_container.sh
@@ -29,3 +29,8 @@ echo "Running 'chezmoi $*'" >&2
 # exec: replace current process with chezmoi
 exec "$chezmoi" "$@"
 
+curl -sS https://starship.rs/install.sh | sh
+curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
+    | bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
+
+chsh -s $(which zsh)
